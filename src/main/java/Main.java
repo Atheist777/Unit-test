49cf3177ef817fcf26d94d4bd00f833549d2e0e9
Atheist777 <102061405+Atheist777@users.noreply.github.com>
2022-08-27 14:12:41 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-
     public static void main(String[] args) {
-        Collection<Person> persons = getRandomPersonsList();
 
+        Collection<Person> persons  = getRandomPersonsList();
+
+        long countYoungPeople = getCountYongPeople(persons);
+        System.out.println("Количество несовершеннолетних: " + countYoungPeople);
+
+        List<String> listFamilySolder = getListFamilySolder(persons);
+
+        System.out.println("Количество призывников: " + listFamilySolder.size());
+
+        List<Person> listManWorkable = getListWorkable(persons, Sex.MAN, 65);
+        List<Person> listWomanWorkable = getListWorkable(persons, Sex.WOMAN, 60);
+
+        List<Person> listPersonWorable = new ArrayList<>();
+        listManWorkable.addAll(listManWorkable);
+        listPersonWorable.addAll(listWomanWorkable);
+
+        System.out.println("Количество трудоспособных людей: " + listManWorkable.size());
     }
 
-//        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
-//        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
-     Collection<Person> persons = new ArrayList<>();
-
-    public static Collection<Person> getRandomPersonsList() {
+    public static Collection<Person>  getRandomPersonsList() {
         Collection<Person> persons = new ArrayList<>();
 
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
@@ -26,38 +37,34 @@ public class Main {
                     Sex.values()[new Random().nextInt(Sex.values().length)],
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
-
-
         }
 
-
-//        for (int i = 0; i < 10_000_000; i++) {
-//            persons.add(new Person(
-//                    names.get(new Random().nextInt(names.size())),
-//                    families.get(new Random().nextInt(families.size())),
-//                    new Random().nextInt(100),
-//                    Sex.values()[new Random().nextInt(Sex.values().length)],
-//                    Education.values()[new Random().nextInt(Education.values().length)]));
-//        }
-
-
-        int agePeople = (int) persons.stream()
-                .filter(x -> x.getAge() > 18)
-                .count();
-        System.out.println(agePeople);
-
-//        List<String> military = persons.stream()
-//                .filter(x -> x.getAge() >= 18 && x.getAge() < 28 && x.getSex() == Sex.MAN)
-//                .map(Person -> Person.getFamily())
-//                .collect(Collectors.toList());
-
-        List<Person> highEducationPeople = persons.stream()
-                .filter(x -> x.getAge() >= 18 && x.getAge() < 61 && x.getEducation() == Education.HIGHER && x.getSex() == Sex.WOMAN
-                        || x.getAge() >= 18 && x.getAge() < 66 && x.getEducation() == Education.HIGHER && x.getSex() == Sex.MAN)
-                .sorted(Comparator.comparing(Person -> Person.getFamily()))
-                .collect(Collectors.toList());
-
-
         return persons;
+    }
+
+    public static List<Person> getListWorkable(Collection<Person> persons, Sex sex, int maxAge) {
+        return persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getSex() == sex)
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= maxAge)
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> getListFamilySolder(Collection<Person> persons) {
+        List<String> listFamilySolder = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        return listFamilySolder;
+    }
+
+    public static long getCountYongPeople(Collection<Person> persons) {
+        return persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
     }
 }
